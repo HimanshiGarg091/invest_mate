1fa8e4647a04439a8cd246544b2598ead4f9e4b3
NishthaS-stack <nishthasinglajob@gmail.com>
2024-09-03 19:08:00 +0530

    update
    repos

---
+++ Frontend/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import HomePage from './Landing page/home/HomePage';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div>
+      <HomePage/>
+    </div>
+
+  </React.StrictMode>
+);
+
