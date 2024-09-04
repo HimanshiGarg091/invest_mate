b29124e43d2b7de67d33bb97bf16dff619d90633
Himanshi <garghimanshi093@gmail.com>
2024-09-04 12:55:09 +0530

    Hero

--- Frontend/src/Landing page/home/Hero.js
+++ Frontend/src/Landing page/home/Hero.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-     );
+    return (
+        <div className='container'>
+            <div className='row'>
+                <img src='media/homeHero.png' alt='hero img' className='mb-5' />
+            </div>
+            <h1>
+                Invest in everything
+            </h1>
+            <p>
+                Online platform to invest in stocks, derivatives, mutual funds, and more
+            </p>
+            <button className='p-3 btn btn-primary fs-5' style={{ width: '20%', margin: '0 auto' }}>
+                Sign Up Now
+            </button>
+        </div>
+    );
 }
 
 export default Hero;
