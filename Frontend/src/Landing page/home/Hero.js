5a11d376c156a987750d0b7885776f9e9efc8ebe
Himanshi <garghimanshi093@gmail.com>
2024-10-09 23:14:13 +0530

    Update Hero.js

--- Frontend/src/Landing page/home/Hero.js
+++ Frontend/src/Landing page/home/Hero.js
@@ -2,19 +2,20 @@ import React from 'react';
 
 function Hero() {
     return (
-        <div className='container'>
-            <div className='row'>
+        <div className='container p-5'>
+            <div className='row text-center'>
                 <img src='media/homeHero.png' alt='hero img' className='mb-5' />
+            
+                <h1 className='mt-5' >
+                    Invest in everything
+                </h1>
+                <p>
+                    Online platform to invest in stocks, derivatives, mutual funds, and more
+                </p>
+                <button className='p-3 btn btn-primary fs-5' style={{ width: '20%', margin: '0 auto' }}>
+                    Sign Up Now
+                </button>
             </div>
-            <h1>
-                Invest in everything
-            </h1>
-            <p>
-                Online platform to invest in stocks, derivatives, mutual funds, and more
-            </p>
-            <button className='p-3 btn btn-primary fs-5' style={{ width: '20%', margin: '0 auto' }}>
-                Sign Up Now
-            </button>
         </div>
     );
 }
