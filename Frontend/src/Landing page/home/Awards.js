5b5859cda6466c6f6e17564e658136d81c206b2f
Himanshi <garghimanshi093@gmail.com>
2024-10-11 00:39:00 +0530

    update awards stats

--- Frontend/src/Landing page/home/Awards.js
+++ Frontend/src/Landing page/home/Awards.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 function Awards() {
     return ( 
-        <h1>Awards</h1>
+        <div className='container mt-5'>
+            <div className='row'>
+                <div className='col-6'></div>
+                    <img src='media/largestBroker.svg'/>
+                <div className='col-6'>
+                    <h1>Largest Stock broker in India</h1>
+                </div>
+            </div>
+        </div>
      );
 
 <h1>Awards</h1>}
--- Frontend/src/Landing page/home/Hero.js
+++ Frontend/src/Landing page/home/Hero.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Hero() {
     return (
-        <div className='container p-5'>
+        <div className='container p-5 mb-5'>
             <div className='row text-center'>
                 <img src='media/homeHero.png' alt='hero img' className='mb-5' />
             
@@ -12,7 +12,7 @@ function Hero() {
                 <p>
                     Online platform to invest in stocks, derivatives, mutual funds, and more
                 </p>
-                <button className='p-3 btn btn-primary fs-5' style={{ width: '20%', margin: '0 auto' }}>
+                <button className='p-3 btn btn-primary fs-5 mb-5' style={{ width: '20%', margin: '0 auto' }}>
                     Sign Up Now
                 </button>
             </div>
