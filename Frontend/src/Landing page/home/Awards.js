a0d454e895c5c2e9fceed6c056ec6e65ff08becb
Himanshi <garghimanshi093@gmail.com>
2024-10-15 00:21:00 +0530

    upadte hero awards

--- Frontend/src/Landing page/home/Awards.js
+++ Frontend/src/Landing page/home/Awards.js
@@ -4,10 +4,31 @@ function Awards() {
     return ( 
         <div className='container mt-5'>
             <div className='row'>
-                <div className='col-6'></div>
+                <div className='col-6 p-5'>
                     <img src='media/largestBroker.svg'/>
-                <div className='col-6'>
+                </div>
+                <div className='col-6 p-5 mt-5'>
                     <h1>Largest Stock broker in India</h1>
+                    <p className='mb-5'>2+ million Zerodha clients contribute to over 15% of all
+                        retail order volumes in India by trading and investing in:
+                    </p>
+                    <div className='row'>
+                        <div className='col-6'>
+                            <ul>
+                                <li>Features and Options</li>
+                                <li>Commodity derivatives</li>
+                                <li>Currency derivatives</li>
+                            </ul>
+                        </div>
+                        <div className='col-6'>
+                            <ul>
+                                <li>Stocks & IPOs</li>
+                                <li>Direct mutual funds</li>
+                                <li>Bonds and Govt. Securities</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <img src='media/pressLogos.png' style={{width:"85%"}}/>
                 </div>
             </div>
         </div>
